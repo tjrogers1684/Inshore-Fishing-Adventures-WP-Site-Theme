0739190921a75a9a68bf0a7e614b807f5496a471
TJ Rogers <tj.rogers7@gmail.com>
2019-03-05 12:40:38 -0500

    - Finesse Article Node

--- single-post.php
+++ single-post.php
@@ -6,27 +6,29 @@
 
 	<div class="content-wrap">
 
+		<?php
+			$categories = wp_get_object_terms( $post->ID, 'category' );
+			$post_meta = get_post_meta( $post->ID );
+			$featured_img_url = get_the_post_thumbnail_url(get_the_ID(),'full');
+			$article_snippet = wp_trim_words( get_the_content(), 15, '...' );
+
+			if ( has_term( 'fishing-report', 'category', $post->ID ) ) {
+				$article_type = '<i class="fas fa-file-invoice"></i> Fishing Report';
+			} else {
+				$article_type = '<i class="fas fa-newspaper"></i> Blog';
+			}
+
+		?>
+
+		<div class="blog-article-node-meta-wrap">
+			<h1 class="page-title"><?php the_title(); ?></h1>
+
+			<p class="blog-article-node-meta">Posted on <?php echo get_the_time( "F j, Y" ); ?> &bull; <?php echo $article_type; ?></p>
+		</div>
+
 		<div class="content-area <?php if ( is_active_sidebar( 'right_sidebar' ) ) { echo 'has-sidebar'; } ?>">
 
 			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-				<?php
-					$categories = wp_get_object_terms( $post->ID, 'category' );
-					$post_meta = get_post_meta( $post->ID );
-					$featured_img_url = get_the_post_thumbnail_url(get_the_ID(),'full');
-					$article_snippet = wp_trim_words( get_the_content(), 15, '...' );
-
-					if ( has_term( 'fishing-report', 'category', $post->ID ) ) {
-						$article_type = '<i class="fas fa-file-invoice"></i> Fishing Report';
-					} else {
-						$article_type = '<i class="fas fa-newspaper"></i> Blog';
-					}
-
-				?>
-
-				<h1 class="page-title"><?php the_title(); ?></h1>
-
-				<p class="blog-article-node-meta">Posted on <?php echo get_the_time( "F j, Y" ); ?> &bull; <?php echo $article_type; ?> </p>
 
 				<?php if ( has_post_thumbnail() ) : ?>
 					<?php $featured_img_url = get_the_post_thumbnail_url(get_the_ID(),'full'); ?>
