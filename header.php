f2d85e887622da15f5ef678af23ccbe917a5eea5
TJ Rogers <tj.rogers7@gmail.com>
2019-03-05 12:23:44 -0500

    - A few Blog edits

--- header.php
+++ header.php
@@ -43,30 +43,6 @@
 				<p class="mobile-nav-close"><a href="#"><i class="fas fa-times"></i></a></p>
 			</div>
 
-			<?php if ( is_singular( 'post' ) ) { ?>
-
-				<?php
-					$categories = wp_get_object_terms( $post->ID, 'category' );
-					$post_meta = get_post_meta( $post->ID );
-					$featured_img_url = get_the_post_thumbnail_url(get_the_ID(),'full');
-					$article_snippet = wp_trim_words( get_the_content(), 15, '...' );
-
-					if ( has_term( 'fishing-report', 'category', $post->ID ) ) {
-						$article_type = '<i class="fas fa-file-invoice"></i> Fishing Report';
-					} else {
-						$article_type = '<i class="fas fa-newspaper"></i> Blog';
-					}
-
-				?>
-
-		    	<div class="feature-wrap">
-					<div class="article-node-meta-conatiner">
-						<h1 class="page-title"><?php the_title(); ?></h1>
-						<p class="blog-article-node-meta">Posted on <?php echo get_the_time( "F j, Y" ); ?> &bull; <?php echo $article_type; ?> </p>
-					</div>
-				</div>
-	    	<?php } ?>
-
 		</div><!-- END HEADER WRAP -->
 
 		<p class="wave-overlay-header"><img src="/wp-content/themes/inshorefishing/images/header-wavy-swirl.png" alt=""></p>
--- single-post.php
+++ single-post.php
@@ -10,6 +10,23 @@
 
 			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
+				<?php
+					$categories = wp_get_object_terms( $post->ID, 'category' );
+					$post_meta = get_post_meta( $post->ID );
+					$featured_img_url = get_the_post_thumbnail_url(get_the_ID(),'full');
+					$article_snippet = wp_trim_words( get_the_content(), 15, '...' );
+
+					if ( has_term( 'fishing-report', 'category', $post->ID ) ) {
+						$article_type = '<i class="fas fa-file-invoice"></i> Fishing Report';
+					} else {
+						$article_type = '<i class="fas fa-newspaper"></i> Blog';
+					}
+
+				?>
+
+				<h1 class="page-title"><?php the_title(); ?></h1>
+						<p class="blog-article-node-meta">Posted on <?php echo get_the_time( "F j, Y" ); ?> &bull; <?php echo $article_type; ?> </p>
+
 				<div class="blog-article-section blog-article-content <?php if ( has_post_thumbnail() ) { echo 'has-blog-image'; } ?>">
 						<?php the_content(); ?>
 				</div>
@@ -20,6 +37,27 @@
 				    <div class="blog-article-section blog-article-image" style="background-image: url(<?php echo $featured_img_url; ?>)">
 				    </div>
 				<?php endif; ?>
+
+				<div class="blog-article-node-social-share">
+					<?php
+
+						$post_meta = get_post_meta( $post->ID );
+						$site_url = get_site_url();
+						$article_url = get_permalink();
+						$article_title = $post->post_title;
+						//echo $siteurl;
+						//echo '<p></p>';
+						//echo $articleurl;
+						//echo '<p></p>';
+
+					?>
+
+					<h3>Share this article</h3>
+
+					<a class="social-share share-facebook" href="http://www.facebook.com/sharer.php?u=<?php echo $article_url; ?>"><i class="fab fa-facebook-square"></i><span>Share on Facebook</span></a>
+					<a class="social-share share-twitter" href="http://twitter.com/intent/tweet?text=<?php echo $article_title . ' ' . $article_url; ?>"><i class="fab fa-twitter-square"></i><span>Tweet This Article</span></a>
+					<a class="social-share share-google" href="https://plus.google.com/share?url=<?php echo $article_url; ?>" class="social-share shareGoogle" count-layout="horizontal"><i class="fab fa-google-plus-square"></i><span>Post to Google+</span></a>
+				</div>
 
 		</div>
 
