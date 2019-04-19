753279c9dd26d55c055a32579b648621d0d37d4f
TJ Rogers <tj.rogers7@gmail.com>
2019-04-19 14:33:50 -0400

    - Testimonials Archive

--- 404.php
+++ 404.php
@@ -1,49 +1,18 @@
 <?php get_header(); ?>
 
-<?php
-	if ( is_active_sidebar( 'subnav_tabs' ) ) {
-		echo '<div class="subnav-tabs">';
-		dynamic_sidebar( 'subnav_tabs' );
-		echo '</div>';
-	}
-?>
-
 <div class="page-wrap">
 
 	<div class="content-wrap">
 
-		<?php
-			if ( is_active_sidebar( 'left_sidebar' ) ) {
-				echo '<div class="sidebar-area">';
-				dynamic_sidebar( 'left_sidebar' );
-				echo '</div>';
-			}
-		?>
+		<h1 class="page-title">Lost at Sea??</h1>
 
 		<div class="content-area <?php if ( is_active_sidebar( 'subnav_sidebar' ) ) { echo 'has-sidebar'; } ?>">
+			<p>Sorry page you're looking for does not exist. We apologize for the inconvenience. Feel free to check out the rates of our trips, contact us, or book a trip.</p>
 
-			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-				<p>We're afraid the page you're looking for does not exist. We apologize for the inconvenience.</p>
-
-			<?php endwhile; else: ?>
-
-				<p><?php _e( 'Sorry, no content found' ); ?></p>
-
-			<?php endif; ?>
-
-		<br class="clearfloat" />
-
+		</div>
 	</div>
-
 </div>
 
-<?php
-if ( is_active_sidebar( 'content_bottom' ) ) {
-	echo '<div class="content-bottom">';
-	dynamic_sidebar( 'content_bottom' );
-	echo '</div>';
-}
-?>
+<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
