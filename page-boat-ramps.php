2ab3ee65cb48ac96b4f7c22e2957661de7c48e1c
Marcus Burnette <marcus@thrivecreativelabs.com>
2019-07-03 11:42:40 -0400

    fixed up boat ramp page links and tweaked styles

--- page-boat-ramps.php
+++ page-boat-ramps.php
@@ -31,7 +31,7 @@
 							$encoded_address = urlencode(get_field('boat_ramp_address_1').' '.get_field('boat_ramp_address_2').' '.get_field('boat_ramp_city').' '.get_field('boat_ramp_state').' '.get_field('boat_ramp_zip_code'));
 						?>
 
-						<a href="https://Users/mburnette/Documents/02-websites/inshore-fishing-adventures.google.com/maps/dir//Bair’s+Cove+Boat+Ramp/">
+						<a href="https://www.google.com/maps/search/<?php echo $encoded_address; ?>" target="_blank" class="boat-ramp-map-link">
 							<img class="boat-ramp-map" src="https://maps.googleapis.com/maps/api/staticmap?center=<?php echo $encoded_address; ?>&zoom=11&scale=1&size=600x400&maptype=roadmap&key=AIzaSyDO41JDPZkwZCLbqi02ZhQhQGOtqaz3RYI&format=png&visual_refresh=true&markers=size:mid%7Ccolor:0xff9e14%7Clabel:%7C<?php echo $encoded_address; ?>" alt="Google Map of <?php echo $get_the_title; ?>">
 						</a>
 
